2982a814d0c0c09f764523c4f3ad4ed226868488
elenabaurkot <baurkot@Baurkots-iMac.fios-router.home>
2020-05-15 11:30:19 -0400

    users model created and route hitting

---
+++ routes/api/users.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+// @route   GET api/users
+// @desc    Test route
+// @access  Public
+router.get('/', (req, res) => res.send('Users route'));
+
+module.exports = router;
