d65dedb797fbde8510e6c8c8046af9db6117bf68
elenabaurkot <baurkot@Baurkots-iMac.fios-router.home>
2020-05-15 19:56:48 -0400

    login validation created

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,8 @@
 const Users = require('../models/Users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { validationResult } = require('express-validator');
 
 const authorizeUser = async (req, res) => {
   try {
@@ -10,6 +14,49 @@ const authorizeUser = async (req, res) => {
   }
 };
 
+// LOGIN USER VALIDATION
+const loginUser = async (req, res) => {
+  //  sends error message for invalid entries
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    // See if user exists
+    let user = await Users.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+    // check if password matches the user's stored password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+    // Return JSON Web Token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 3600 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
 module.exports = {
   authorizeUser,
+  loginUser,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,27 @@
 const router = require('express').Router();
-const { authorizeUser } = require('../../controllers/auth-controller');
+const { check } = require('express-validator');
 const auth = require('../../middleware/auth');
+const {
+  authorizeUser,
+  loginUser,
+} = require('../../controllers/auth-controller');
 
 // @route       GET api/auth
 // @desc        Test route
 // @access      Public
 router.route('/').get(auth, authorizeUser);
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router
+  .route('/')
+  .post(
+    [
+      check('email', 'Please include a valid email').isEmail(),
+      check('password', 'Password is required').exists(),
+    ],
+    loginUser
+  );
 
 module.exports = router;
