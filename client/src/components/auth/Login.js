6d0613094f8cf99bc724d5fe2492b048a41b307e
elenabaurkot <elenabaurkot@gmail.com>
2020-05-18 11:10:12 -0400

    login and registration pages created (not yet styled)

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from  'react-router-dom'; 
 
 const Login = () => {
-    return (
-        <div>
-           Login 
-        </div>
-    )
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData; 
+
+    const onChange = e => 
+        setFormData({...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log('Success');
+    };
+
+    return <Fragment>
+        <h1>Sign In</h1>
+        <form className='form' onSubmit={e => onSubmit(e)}>
+            <div className="form-group">
+                <input 
+                    type="email" 
+                    placeholder="Email Address" 
+                    name="email" 
+                    value={email} 
+                    onChange={e => onChange(e)}
+                    required 
+                />
+            </div>
+            <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password} 
+                    onChange={e => onChange(e)}
+                    minLength="6"
+                />
+            </div>
+            <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p>
+            Don't have an account? <Link to='/register'>Sign Up</Link>
+        </p>
+    </Fragment>
 }
 
-export default Login
+export default Login;
