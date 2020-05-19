2c1d76fd9bd82d20320cea26f9eafa8de26bd8bb
elenabaurkot <elenabaurkot@gmail.com>
2020-05-19 01:54:43 -0400

    navbar and landing page background pic styling added

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../styles/landing.css';
+
 const Landing = () => {
     return (
-        <div>
-            Landing
+    <section className="landing">
+        <div className="text">
+            <section className='pic-text'>
+                <p>Welcome to Feast With Friends</p>
+                <p>A website where you can create a recipe book so you never forget how to make your favorite family foods and where you can checkout recipes of your friends and followers.</p>
+                <Link to='#'>Sign In</Link>
+                <Link to='#'>Sign Up</Link>
+            </section>
         </div>
+    </section>
     )
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../styles/navbar.css'
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="navbar bg-dark">
+        <h1>
+            <Link to= '/' className='noDec oLink'>
+                <h1 id='navHeader'>
+                    <i class="fas fa-pepper-hot"></i>&nbsp; 
+                    Feast With Friends
+                </h1>
+            </Link>
+        </h1>
+        <ul>
+            <li>
+               <Link to="/register" className='noDec oLink navLink'>Register</Link>
+            </li>
+            <li>
+                <Link to="/login" className='noDec oLink navLink'>Login</Link>
+            </li>
+        </ul>
+        </nav>
     )
 }
 
